d5bd898586ded5af62ccfdcbb54c3603fb84d41e
Rhymes Toasterface <rhyme_master@hotmail.co.uk>
2017-02-28 16:23:41 +0000

    Add authentication middleware to verify a token

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/setup', function(req, res) {
 // get an instance of the router for api routes
 var apiRoutes = express.Router();
 
-// TODO: route to authenticate a user (POST http://localhost:8000/api/authenticate)
+// route to authenticate a user (POST http://localhost:8000/api/authenticate)
 apiRoutes.post('/authenticate', function(req, res) {
 
     // find the user
@@ -96,7 +96,38 @@ apiRoutes.post('/authenticate', function(req, res) {
 
 });
 
-// TODO: route middleware to verify a token
+// route middleware to verify a token
+apiRoutes.use(function(req, res, next) {
+
+   // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt. verify(token, app.get('superSecret'), function(err, decoded) {
+            if (err) {
+                return res.json({ sucess: false, message: 'Failed to authenticate token.'});
+            } else {
+                //if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+
+});
 
 // route to show a random message
 apiRoutes.get('/', function(req, res) {
